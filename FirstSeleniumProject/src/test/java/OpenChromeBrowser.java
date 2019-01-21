56a3738cc7abdabeea7d329e5e6e703b8f8300ed
AlexandrNenadikh <46789168+AlexandrNenadikh@users.noreply.github.com>
2019-01-21 20:29:37 +0200

    Classwork

--- FirstSeleniumProject/src/test/java/OpenChromeBrowser.java
+++ FirstSeleniumProject/src/test/java/OpenChromeBrowser.java
@@ -5,6 +5,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class OpenChromeBrowser {
 
 
@@ -13,13 +15,18 @@ public class OpenChromeBrowser {
     @BeforeClass
     public void setUp(){
     wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
     }
     @Test
     public void chromeBrowserTest(){
         wd.get("http://google.com");
-        wd.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Look, i am testing myself");
-        wd.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[3]/center/input[1]")).click();
+//        wd.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Look, i am testing myself");
+//        wd.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[3]/center/input[1]")).click();
+        wd.findElement(By.name("q")).click();
+        wd.findElement(By.name("q")).sendKeys("Рефакторить");
+        wd.findElement(By.name("btnK")).click();
+
     }
     @AfterClass
     public void tearDown() throws InterruptedException {
---
+++ FirstSeleniumProject/src/test/java/OpenFirefoxBrowser.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class OpenFirefoxBrowser {
+
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void testeBaySearch(){
+        wd.get("https://www.ebay.com/");
+        wd.findElement(By.id("gh-ac")).click();
+        wd.findElement(By.id("gh-ac")).clear();
+        wd.findElement(By.id("gh-ac")).sendKeys("glasses");
+        wd.findElement(By.id("gh-btn")).click();
+
+    }
+    @AfterClass
+    public void tearDown(){
+//        Thread.sleep(10500);
+//      wd.quit();
+    }
+}
