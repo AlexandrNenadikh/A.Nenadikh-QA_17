a6f970b8ffa7bf6f9c64b1b42f32f2d3225fc2f4
AlexandrNenadikh <46789168+AlexandrNenadikh@users.noreply.github.com>
2019-01-23 17:54:52 +0200

    History of destruction

--- 1234/src/main/java/class1.java
+++ 1234/src/main/java/class1.java
@@ -1,14 +1,13 @@
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.firefox.FirefoxDriver;
+        import org.testng.annotations.AfterClass;
+        import org.testng.annotations.BeforeClass;
+        import org.testng.annotations.Test;
 
-public class OpenFirefoxBrowser {
+        import java.util.concurrent.TimeUnit;
 
+public class class1 {
 
     WebDriver wd;
 
