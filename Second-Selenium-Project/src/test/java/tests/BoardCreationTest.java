887d23da9c57ed850dcbc41187d9d408aace990a
AlexandrNenadikh <46789168+AlexandrNenadikh@users.noreply.github.com>
2019-02-14 13:27:10 +0200

    14 02

---
+++ Second-Selenium-Project/src/test/java/tests/BoardCreationTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BoardCreationTest extends TestBase {
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (!app.getSessionHelper().isUserLoggedIn()) {
+            app.getSessionHelper().login();
+        }
+    }
+
+    @Test
+    public void testBoardCreationFromBody() {
+
+        app.getBoardHelper().clickTheCreateNewBoardOnTheEndOfList();
+
+        app.getBoardHelper().addBoardTitle("newBoard" + System.currentTimeMillis());
+        app.getBoardHelper().clickTheCreateButton();
+        app.returnToPreviousPage();
+    }
+
+    @Test
+    public void testBoardCreationFromHeader() {
+        app.clickOnPlusButtonOnHeader();
+        app.getBoardHelper().selectCreateBoard();
+        app.getBoardHelper().addBoardTitle("newBoard" + System.currentTimeMillis());
+        app.getBoardHelper().clickTheCreateButton();
+        app.returnToPreviousPage();
+
+    }
+}
